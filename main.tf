a5678bd2640a91fd9ee2663bdaf94ddc3d6cd63d
hassan <hassantariq180@gmail.com>
2024-04-02 14:40:34 +0500

    create a new subnet in the existing vpc

--- main.tf
+++ main.tf
@@ -13,10 +13,26 @@ resource "aws_vpc" "dev-vpc" {
 
 // create a new subnet for the above vpc
 resource "aws_subnet" "dev-subnet" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.0.0/20"
 
   tags = {
     Name = "dev-subnet"
   }
 }
+
+// query the existing default vpc id from data block
+data "aws_vpc" "existing" {
+  default = true
+}
+
+// creating a new subnet in the existing vpc
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id     = data.aws_vpc.existing.id
+  cidr_block = "172.31.48.0/20"
+
+  tags = {
+    Name = "dev-subnet-2"
+  }
+}
+
