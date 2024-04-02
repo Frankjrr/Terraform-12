f6d887b8b3397e55bd8fa60256e1d54c06f10d2d
hassan <hassantariq180@gmail.com>
2024-04-02 14:34:59 +0500

    create a new vpc and its subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 provider "aws" {
+  // configuration files here
+}
 
+// create a new vpc
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+// create a new subnet for the above vpc
+resource "aws_subnet" "dev-subnet" {
+  vpc_id = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.0.0/20"
+
+  tags = {
+    Name = "dev-subnet"
+  }
 }
