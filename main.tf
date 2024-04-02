40d8076e2eba979d3d7535bb6ff15a143b8cb7c4
hassan <hassantariq180@gmail.com>
2024-04-02 15:38:56 +0500

    variable file added

--- main.tf
+++ main.tf
@@ -2,19 +2,29 @@ provider "aws" {
   // configuration files here
 }
 
+variable "subnet-cidr-block" {
+  // add description
+  description = "subnet cidr block"
+  //add default file value in case it didnt file with name = terraform.tfvars
+  //default = "10.0.0.0/20"
+  // =====================
+  // define type of the variable as a constraints
+  // type = list(string) // lsit of strings
+}
+
 // create a new vpc
 resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "dev-vpc"
+    Name = "development"
   }
 }
 
 // create a new subnet for the above vpc
 resource "aws_subnet" "dev-subnet" {
   vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = "10.0.0.0/20"
+  cidr_block = var.subnet-cidr-block
 
   tags = {
     Name = "dev-subnet"
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+// varibale name from the main.tf file
+subnet-cidr-block = "10.0.0.0/20"
